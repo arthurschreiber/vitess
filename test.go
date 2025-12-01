d61dd285fe3589fe2d8aa1b8396cd12c53529220
Arthur Schreiber <arthur@planetscale.com>
2025-12-01 10:45:30 +0000

    Remove use and definition of `GetFlagVariantForTests` helper.
    Signed-off-by: Arthur Schreiber <arthur@planetscale.com>

--- test.go
+++ test.go
@@ -185,7 +185,7 @@ func (t *Test) run(dir, dataDir string) ([]byte, error) {
 				testCmd = append(testCmd, "-keep-data")
 			}
 		} else {
-			testCmd = []string{"test/" + t.File, "-v", "--skip-build", utils.GetFlagVariantForTests("--keep-logs")}
+			testCmd = []string{"test/" + t.File, "-v", "--skip-build", "--keep-logs"}
 			testCmd = append(testCmd, t.Args...)
 		}
 		if *partialKeyspace {
